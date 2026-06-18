aa1ad7baf2e0abb27af4c36d64d2982ec5319daf
celand2 <ndikuceland@gemail.com>
2026-06-18 20:34:44 +0200

    pwa corrige

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,4 +6,5 @@ use App\Providers\FortifyServiceProvider;
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
+    LaravelPWA\Providers\LaravelPWAServiceProvider::class,
 ];
