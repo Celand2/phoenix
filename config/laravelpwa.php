bf937f7952540b2faae367dc2b47eadb6c28fefb
celand2 <ndikuceland@gemail.com>
2026-06-18 20:07:25 +0200

    pwa installe

---
+++ config/laravelpwa.php
@@ -0,0 +1,38 @@
+<?php
+return [
+    'name' => 'phenix Traders',
+    'manifest' => [
+        'name'             => 'phenix Traders',
+        'short_name'       => 'Phenix',
+        'start_url'        => '/',
+        'background_color' => '#dbb91042',
+        'theme_color'      => '#000000',
+        'display'          => 'standalone',
+        'orientation'      => 'portrait',
+        'status_bar'       => 'black',
+        'icons' => [
+            '72x72'   => ['path' => '/images/icons/icon-72x72.png',   'purpose' => 'any maskable'],
+            '96x96'   => ['path' => '/images/icons/icon-96x96.png',   'purpose' => 'any maskable'],
+            '128x128' => ['path' => '/images/icons/icon-128x128.png', 'purpose' => 'any maskable'],
+            '144x144' => ['path' => '/images/icons/icon-144x144.png', 'purpose' => 'any maskable'],
+            '152x152' => ['path' => '/images/icons/icon-152x152.png', 'purpose' => 'any maskable'],
+            '192x192' => ['path' => '/images/icons/icon-192x192.png', 'purpose' => 'any maskable'],
+            '384x384' => ['path' => '/images/icons/icon-384x384.png', 'purpose' => 'any maskable'],
+            '512x512' => ['path' => '/images/icons/icon-512x512.png', 'purpose' => 'any maskable'],
+        ],
+        'splash' => [
+        '640x1136' => '/images/icons/apple-splash-640-1136.png',
+        '750x1334' => '/images/icons/apple-splash-750-1334.png',
+        '1242x2208' => '/images/icons/apple-splash-1242-2208.png',
+        '1125x2436' => '/images/icons/apple-splash-1125-2436.png',
+        '828x1792' => '/images/icons/apple-splash-828-1792.png',
+        '1242x2688' => '/images/icons/apple-splash-1242-2688.png',
+        '1536x2048' => '/images/icons/apple-splash-1536-2048.png',
+        '1668x2224' => '/images/icons/apple-splash-1668-2224.png',
+        '1668x2388' => '/images/icons/apple-splash-1668-2388.png',
+        '2048x2732' => '/images/icons/apple-splash-2048-2732.png',
+    ],
+        'shortcuts' => [],
+        'custom' => [],
+    ],
+];
--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>@yield('title', 'Client') - phenix Traders</title>
+    @laravelPWA
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="{{ asset('js/client.js') }}" defer></script>
 </head>
